62efa6a1d36f08b749c537a5b306a394f724cb23
Ken Takagi <tkg214@gmail.com>
2017-04-14 15:27:33 -0700

    add dialog for create message in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,79 @@
 import React, { Component } from 'react';
 import {List, ListItem} from 'material-ui/List';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 import CreateIcon from 'material-ui/svg-icons/content/add-circle';
 import '../index.css';
 
 class Sidebar extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      open: false,
+    }
+  }
+
+  _handleOpen() {
+    this.setState({open: true});
+  };
+
+  _handleClose() {
+    this.setState({
+      open: false,
+    });
+  };
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    const messageInput = document.getElementById('message-input')
+    console.log(messageInput.value)
+    this.setState({
+      open: false,
+    });
+  };
+
   render() {
+
+    const openActions = [
+      <FlatButton
+        label='Cancel'
+        primary={true}
+        onTouchTap={this._handleClose.bind(this)}
+      />,
+      <FlatButton
+        label='Submit'
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this._handleSubmit.bind(this)}
+      />,
+    ];
+
     return (
       <nav className='sidebar'>
         <List>
           <ListItem
             key={1}
             primaryText='Create Message'
+            onTouchTap={this._handleOpen.bind(this)}
             leftIcon={<CreateIcon />}
           />
         </List>
+        <Dialog
+          title='Create Message'
+          actions={openActions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this._handleClose.bind(this)}
+        >
+          <TextField
+            hintText='Enter your message'
+            id='message-input'
+            multiLine={true}
+            fullWidth={true}
+          />
+        </Dialog>
       </nav>
     )
   }
